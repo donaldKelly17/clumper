d198c36cce53a18ab538155746634bbb2d4b1ea2
donaldkelly17 <donaldkelly17@gmail.com>
2021-01-10 17:53:25 +0000

    parametrized collect tail test

--- tests/test_verbs/test_headtail.py
+++ tests/test_verbs/test_headtail.py
@@ -8,11 +8,10 @@ def test_headtail_size(base_clumper, n, expected):
     assert len(base_clumper.tail(n)) == expected
 
 
-def test_can_collect_head():
-    data = [{"i": i} for i in range(10)]
-    c = Clumper(data).head(1)
-    collected = c.collect()
-    assert collected[0]["i"] == 0
+@pytest.mark.parametrize("idx", [i for i in range(1, 26)])
+def test_can_collect_head(base_clumper, idx):
+    collected = base_clumper.head(idx).collect()
+    assert collected[-1]["i"] == idx - 1
 
 
 def test_can_collect_tail():
