af032e121e5817ebe62bf7f81530606dfd434456
donaldkelly17 <donaldkelly17@gmail.com>
2021-01-10 17:35:05 +0000

    reordered tests. swopped 'head' and 'tail'

--- tests/test_verbs/test_headtail.py
+++ tests/test_verbs/test_headtail.py
@@ -8,15 +8,15 @@ def test_headtail_size(base_clumper, n, expected):
     assert len(base_clumper.tail(n)) == expected
 
 
-def test_can_collect_tail():
-    data = [{"i": i} for i in range(10)]
-    c = Clumper(data).tail(1)
-    collected = c.collect()
-    assert collected[0]["i"] == 9
-
-
 def test_can_collect_head():
     data = [{"i": i} for i in range(10)]
     c = Clumper(data).head(1)
     collected = c.collect()
     assert collected[0]["i"] == 0
+
+
+def test_can_collect_tail():
+    data = [{"i": i} for i in range(10)]
+    c = Clumper(data).tail(1)
+    collected = c.collect()
+    assert collected[0]["i"] == 9
